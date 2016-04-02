5aa3c729902bc8ddf1a936e5fa07121fb1dd9ac3
Vladimir <v.s.vorotnikov@gmail.com>
2016-04-02 13:19:15 +0300

    additional info to HTML-template

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import webbrowser
 import os
 import re
@@ -9,7 +11,7 @@ main_page_head = '''
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Fresh Tomatoes!</title>
+    <title>P1 Udacity FSND (Movie Trailers)</title>
 
     <!-- Bootstrap 3 -->
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
@@ -107,7 +109,7 @@ main_page_content = '''
       <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
           <div class="navbar-header">
-            <a class="navbar-brand" href="#">Fresh Tomatoes Movie Trailers</a>
+            <a class="navbar-brand" href="#">P1 Udacity FSND (Movie Trailers)</a>
           </div>
         </div>
       </div>
@@ -125,6 +127,9 @@ movie_tile_content = '''
 <div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
     <img src="{poster_image_url}" width="220" height="342">
     <h2>{movie_title}</h2>
+    <h4>Director: {movie_director}</h4>
+    <p>IMDB rating: {movie_imdb_rating} ★</p>
+    <p>Stars: {movie_stars}</p>
 </div>
 '''
 
@@ -140,12 +145,19 @@ def create_movie_tiles_content(movies):
             r'(?<=be/)[^&#]+', movie.trailer_youtube_url)
         trailer_youtube_id = (youtube_id_match.group(0) if youtube_id_match
                               else None)
+        stars = ""
+        for i in movie.stars:
+            stars += i + ", "
+        stars = stars[:-2]
 
         # Append the tile for the movie with its content filled in
         content += movie_tile_content.format(
             movie_title=movie.title,
             poster_image_url=movie.poster_image_url,
-            trailer_youtube_id=trailer_youtube_id
+            trailer_youtube_id=trailer_youtube_id,
+            movie_director=movie.director,
+            movie_imdb_rating=movie.IMDB_rating,
+            movie_stars=stars
         )
     return content
 
